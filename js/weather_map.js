6986f5e1706d49e5a649aeba4653b05f671b07c8
Ron Soeum <ronsoeum@gmail.com>
2018-12-17 01:13:51 -0600

    added new theme, fixed clicked map markers

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,27 +1,239 @@
 "use strict";
 
 $(document).ready(function(){
-
+    // Initial Lat Lng for Map Position and Weather Info
     var latitude = 29.4241,
         longitude = -98.4936;
 
+
     // Google Maps API
     (function map() {
+        // Render Google Maps API
         var map = new google.maps.Map(document.getElementById("map"), {
             center: {lat: latitude, lng: longitude},
             zoom: 11,
-            mapTypeId: 'roadmap'
+            mapTypeId: 'roadmap',
+            styles: [
+                {
+                    "featureType": "administrative",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#444444"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "color": "#f2f2f2"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 45
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "visibility": "simplified"
+                        },
+                        {
+                            "color": "#ff6a6a"
+                        },
+                        {
+                            "lightness": "0"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#ee3123"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "geometry.stroke",
+                    "stylers": [
+                        {
+                            "color": "#ee3123"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "labels.text",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#ee3123"
+                        },
+                        {
+                            "lightness": "62"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.local",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "lightness": "75"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit.line",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit.station.bus",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit.station.rail",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit.station.rail",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "weight": "0.01"
+                        },
+                        {
+                            "hue": "#ff0028"
+                        },
+                        {
+                            "lightness": "0"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "on"
+                        },
+                        {
+                            "color": "#80e4d8"
+                        },
+                        {
+                            "lightness": "25"
+                        },
+                        {
+                            "saturation": "-23"
+                        }
+                    ]
+                }
+            ]
         });
+
 
         // Search Box
         var input = document.getElementById('pac-input');
         var searchBox = new google.maps.places.SearchBox(input);
         map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
-
         // Bias the SearchBox results towards current map's viewport.
         map.addListener('bounds_changed', function () {
             searchBox.setBounds(map.getBounds());
         });
+
 
         // InfoWindow for Codeup
         var codeup = { lat: 29.4267857, lng: -98.4896021 };
@@ -30,7 +242,7 @@ $(document).ready(function(){
             + '<div class="iw-content">'
             + '<div class="iw-subTitle">Reboot Your Career</div>'
             + '<img src="images/codeup.jpg" alt="Codeup Building" height="180" width="180">'
-            + '<p>Codeup offers 18-week accelerator programs to launch your career as a ' +
+            + '<p class="mb-3">Codeup offers 18-week accelerator programs to launch your career as a ' +
             'computer programmer or a data scientist.</p>'
             + '<div class="iw-subTitle">Contacts</div>'
             + '<p class="mb-0">600 Navarro St #350<br>San Antonio, TX 78205<br>'
@@ -39,55 +251,60 @@ $(document).ready(function(){
         var codeupInfoWindow = new google.maps.InfoWindow({
             content: codeupContent,
             maxWidth: 440
-        });// InfoWindow for Codeup
-
+        });
         // Marker for Codeup
         var codeupMarker = new google.maps.Marker({
             animation: google.maps.Animation.DROP,
             position: codeup,
             map: map,
-            icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
             title: 'codeup'
         });
-        // Open InfoWindow
+        // Open Codeup InfoWindow
         google.maps.event.addListener(codeupMarker, 'click', function() {
             codeupInfoWindow.open(map, codeupMarker);
         });
-        // Close InfoWindow
+        // Close Codeup InfoWindow
         google.maps.event.addListener(map, 'click', function() {
             codeupInfoWindow.close();
-        });// Marker for Codeup
-
-        // Click Markers
-        map.addListener('click', function(e) {
-            placeMarker(e.latLng, map);
         });
 
-        function placeMarker(position, map) {
+
+        var markers = [];
+        // Adds a marker to the map and pushes it into markers array
+        function addNewMarkers(position) {
             var marker = new google.maps.Marker({
                 position: position,
                 map: map
             });
+            markers.push(marker);
             map.panTo(position);
         }
-
-        // Search Markers
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function () {
-            var places = searchBox.getPlaces();
-
-            if (places.length == 0) {
-                return;
-            }
-
-            // Clear out the old markers.
+        // Clear out the old markers.
+        function deleteMarker() {
             markers.forEach(function (marker) {
                 marker.setMap(null);
             });
             markers = [];
-
+        }
+        // Click to Create Map Marker
+        map.addListener('click', function(event) {
+            deleteMarker();
+            addNewMarkers(event.latLng);
+            // Get Longitude and Latitude
+            latitude = event.latLng.lat();
+            longitude = event.latLng.lng();
+            // Change Weather API
+            $('.weatherBox1').empty();
+            $('.weatherBox2').empty();
+            $('.weatherBox3').empty();
+            weather(latitude, longitude);
+        });
+        // Search Markers
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function () {
+            var places = searchBox.getPlaces();
+            deleteMarker();
             // For each place, get the icon, name and location.
             var bounds = new google.maps.LatLngBounds();
             places.forEach(function (place) {
@@ -95,34 +312,23 @@ $(document).ready(function(){
                     console.log("Returned place contains no geometry");
                     return;
                 }
-                var icon = {
-                    url: place.icon,
-                    size: new google.maps.Size(71, 71),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17, 34),
-                    scaledSize: new google.maps.Size(25, 25)
-                };
-
                 // Create a marker for each place.
                 markers.push(new google.maps.Marker({
                     animation: google.maps.Animation.DROP,
                     map: map,
-                    icon: icon,
                     title: place.name,
                     position: place.geometry.location
                 }));
-
+                // Set new map bounds
                 if (place.geometry.viewport) {
                     // Only geocodes have viewport.
                     bounds.union(place.geometry.viewport);
                 } else {
                     bounds.extend(place.geometry.location);
                 }
-
-                // Get Location
+                // Get Longitude and Latitude
                 latitude = place.geometry.location.lat().toFixed(3);
                 longitude = place.geometry.location.lng().toFixed(3);
-
                 // Change Weather API
                 $('.weatherBox1').empty();
                 $('.weatherBox2').empty();
@@ -130,55 +336,15 @@ $(document).ready(function(){
                 weather(latitude, longitude);
             });
             map.fitBounds(bounds);
-            searchBox.set('map', map);
-        });// Search Markers
-
-
-        // Get long & lat
-        // google.maps.event.addListener(searchBox, 'places_changed', function() {
-        //     searchBox.set('map', null);
-        //
-        //     var places = searchBox.getPlaces();
-        //
-        //     var bounds = new google.maps.LatLngBounds();
-        //     var i, place;
-        //     for (i = 0; place = places[i]; i++) {
-        //         (function(place) {
-        //             var marker = new google.maps.Marker({
-        //                 position: new google.maps.LatLng(latitude, longitude),
-        //                 draggable: true,
-        //                 position: place.geometry.location
-        //             });
-        //             marker.bindTo('map', searchBox, 'map');
-        //             google.maps.event.addListener(marker, 'map_changed', function() {
-        //                 if (!this.getMap()) {
-        //                     this.unbindAll();
-        //                 }
-        //             });
-        //             bounds.extend(place.geometry.location);
-        //
-        //             map.addListener('bounds_changed', function(evt) {
-        //                 // document.getElementById('long-lat').innerHTML = evt.latLng.lat().toFixed(3) + " " + evt.latLng.lng().toFixed(3);
-        //                 // console.log(place.geometry.location.lat().toFixed(3) + " " + place.geometry.location.lng().toFixed(3));
-        //                 searchPositionLat = place.geometry.location.lat().toFixed(3);
-        //                 searchPositionLong = place.geometry.location.lng().toFixed(3);
-        //             });
-        //         }(place));
-        //     }
-        //     map.fitBounds(bounds);
-        //     searchBox.set('map', map);
-        //     map.setZoom(Math.min(map.getZoom(), 12));
-        // });
-
+        });
     })();// Google Maps API
 
 
     // Weather Map API
     var weather = function() {
-
         var callWeatherAPI = "http://api.openweathermap.org/data/2.5/forecast?lat="
             + latitude + "&lon=" + longitude + "&APPID=490c989b60b09d95886b0ecd1b824208";
-
+        // Wind Direction
         var windDirection = function(degree){
             if(degree>337.5) return 'N';
             if(degree>292.5) return 'NW';
@@ -190,23 +356,32 @@ $(document).ready(function(){
             if(degree>22.5){return 'NE';}
             return 'N';
         };
-
+        // Call OpenWeather API
         $.get(callWeatherAPI, {
             units: "imperial"
         })
             .done(function (data) {
+                // Day of Week
+                var dayOfWeek = function(index) {
+                    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                    var dayNum = new Date(data.list[index].dt * 1000).getDay();
+                    return days[dayNum];
+                };
+                // Icon URL
                 var iconCode = data.list[7].weather[0].icon,
                     iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
-
+                // Location Name
                 $('#location').html(data.city.name);
-                var renderWeather = function (weatherBox, day) {
-                    weatherBox.html("<p class='mt-3'>" + data.list[day].main.temp_max + "&deg;\/"
-                        + data.list[day].main.temp_min + "&deg;" + "</p>"
-                        + "<div>" + "<img src=" + iconUrl + ">" + "</div>"
-                        + "<p class='d-inline-block mr-1'>" + "<strong>" + data.list[day].weather[0].main + ":" + "</strong>" + "</p>"
+                // Render Weather Info
+                var renderWeather = function(weatherBox, day) {
+                    weatherBox.html("<div class='row'>" + "<div class='col-6 p-0'>"
+                        + "<h4 class='mt-2'>" + dayOfWeek(day) + "</h4>" + "</div>"
+                        + "<div class='col-3 p-0'>" + "<h4 class='mt-2'>" + Number(data.list[day].main.temp.toFixed(0)) + "&deg;" + "</h4>" + "</div>"
+                        + "<div class='col-3 p-0'>" + "<img src=" + iconUrl + ">" + "</div>" + "</div>"
+                        + "<p class='d-inline-block mr-1'>" + data.list[day].weather[0].main + ":" + "</p>"
                         + "<p class='d-inline-block'>" + data.list[day].weather[0].description + "</p>"
                         + "<p>" + "Humidity: " + data.list[day].main.humidity + "%" + "</p>"
-                        + "<p>" + "Wind: " + windDirection(data.list[day].wind.degree) + " " + data.list[day].wind.speed + " mph" + "</p>"
+                        + "<p>" + "Wind: " + windDirection(data.list[day].wind.degree) + " " + Math.round(data.list[day].wind.speed) + " mph" + "</p>"
                         + "<p class='mb-3'>" + "Pressure: " + data.list[day].main.pressure + "</p>");
                 };
                 renderWeather($('.weatherBox1'), 7);
@@ -216,8 +391,8 @@ $(document).ready(function(){
             .fail(function (data, status) {
                 console.log(status);
                 alert('weather status: ' + status);
-            });// Weather Map API
-    };
+            });
+    };// Weather Map API
     weather();
 
-});//--ready--
+});//ready
