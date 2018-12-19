f64ce30b5b2fea1b640fcc990a1f9d37fce367f4
Ron Soeum <ronsoeum@gmail.com>
2018-12-19 11:23:32 -0600

    refactor code with new solution

--- js/promises.js
+++ js/promises.js
@@ -6,34 +6,21 @@ $(document).ready(function(){
     const wait = time => new Promise(resolve => setTimeout(resolve, time));
     wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
-    // UserInfo
-    const getGithubUserInfo = function(user){
-        return fetch(`https://api.github.com/users/${user}`,
-            {headers: {'Authorization': 'token KEY'}})
-            .then(response => response.json())
-            .then(user => console.log(user))
-            .catch(error => console.error(error));
-    };
-    getGithubUserInfo("RonSoeum");
 
-    // User Repo List
-    const getGithubUserRepoList = function(user){
-        fetch(`https://api.github.com/users/${user}/repos`,
-            {headers: {'Authorization': 'token KEY'}})
-            .then(response => response.json())
-            .then(repo => console.log(repo))
-            .catch(error => console.error(error));
-    };
-    getGithubUserRepoList("RonSoeum");
+    // Fetch Last Commit
+    const API_TOKEN = 'KEY';
 
-    // User Repo commit
-    const getGithubUserRepoCommit = function (user){
-        fetch(`https://api.github.com/repos/${user}/codeup-web-exercises/commits`,
-            {headers: {'Authorization': 'token KEY'}})
+    const fetchOptions = {
+        headers: {'Authorization': `token ${API_TOKEN}`}
+    };
+
+    const getGithubUserLastCommit = function(user){
+        return fetch(`https://api.github.com/users/${user}/events`, fetchOptions)
             .then(response => response.json())
-            .then(commit => console.log(commit[0].commit.author.date))
+            .then(lastCommit => lastCommit[0].created_at)
+            .then(lastCommit => console.log(lastCommit))
             .catch(error => console.error(error));
     };
-    getGithubUserRepoCommit("RonSoeum");
+    getGithubUserLastCommit("RonSoeum");
 
 });//ready
